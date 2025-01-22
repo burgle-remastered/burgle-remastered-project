ed286015af04765c91ec131c6259e573fe1c0942
madisonnnn <madisontolentino@gmail.com>
2025-01-22 11:24:10 -0500

    pushing

--- burgle-backend/app/__init__.py
+++ burgle-backend/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models import *
+
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -18,7 +20,7 @@ def create_app():
     migrate.init_app(app, db)
 
     # Register blueprints or routes
-    from app.routes import main_bp
+    from routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
--- burgle-backend/run.py
+++ burgle-backend/run.py
@@ -1,6 +1,16 @@
 from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate
+from app import models
 
 app = create_app()
 
+# Register Migrate with the app
+migrate = Migrate(app, db)
+
+# For Flask CLI to recognize Migrate commands
+print("App instance created:", app)
+print("Extensions:", app.extensions)
 if __name__ == "__main__":
     app.run(debug=True)
+    
