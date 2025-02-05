344c78a5df64f8be89aea216bd7f490d9ad91eb3
madisonnnn <madisontolentino@gmail.com>
2025-02-05 17:13:05 -0500

    updates

--- burgle-frontend/src/main.jsx
+++ burgle-frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import CurrentUserContextProvider from './contexts/current-user-context.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <CurrentUserContextProvider>
+       <App />
+    </CurrentUserContextProvider>
   </StrictMode>
 )
